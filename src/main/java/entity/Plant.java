be6e2d804ed2a910d92d3de07d90c84e0d48836a
dusanov <sadriddindusanov@gmail.com>
2026-04-18 10:06:43 +0500

    Plant and PlantType are created

---
+++ src/main/java/entity/Plant.java
@@ -0,0 +1,31 @@
+package entity;
+
+import enums.PlantType;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+
+@Entity
+public class Plant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "latin_name")
+    private String latinName;
+    @Column(name = "common_name")
+    private String commonName;
+    @Column
+    private String code;
+    @Enumerated(EnumType.STRING)
+    private PlantType type;
+    @Column(name = "exotic")
+    private Boolean isExotic;
+    @Column(name = "flowering")
+    private Boolean isFlowering;
+    @Column(name = "flower_color")
+    private String flowerColor;
+}
